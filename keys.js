174c26ef22d0162ddb976e2ca1edff1bb076cd86
asimeoli <asimeoli@gmail.com>
2017-08-28 18:37:18 -0400

    should have done this sooner

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'qWYqz18l12M0AqMoH2p18i9Xa',
+    consumer_secret: 'AALngvjLiI38aqMMmFT8NQkw7nqFPWrbusIKhlJMnWhibzEx8v',
+    access_token: '900106973861085184-OOODyIvq1hH9OJNJX0gcPhDxWhikf7c',
+    access_token_secret: 'mytng37olJ3AYB5pIOnCKXxKvE0vXjNaRpwof0cfMWWdC',
+}
---
+++ liri.js
@@ -0,0 +1,188 @@
+// 7. At the top of the `liri.js` file, write the code you need to grab the data from keys.js. 
+// Then store the keys in a variable.
+
+// var keys = require("./keys.js");
+
+// 8. Make it so liri.js can take in one of the following commands:
+var request = require("./package.json/node_modules/request");
+var inquirer = require("./package.json/node_modules/inquirer");
+var twit = require("./package.json/node_modules/twit")
+var keys = require("./keys");
+var Spotify = require('./package.json/node_modules/node-spotify-api');
+
+//var fs = require("./package.json/node_modules/fs")
+
+function runText() {
+    console.log("text file")
+}
+
+//Twitter request
+function twitter() {
+    //attached twitter keys
+    var T = new twit(keys)
+
+    var params = {
+        screen_name: 'ann_bootcamp',
+        count: 20
+    }
+
+
+    T.get('statuses/user_timeline', params, gotData);
+
+    function gotData(err, data, response) {
+        var tweets = data;
+        for (var i = 0; i < tweets.length; i++) {
+            console.log("############################################################");
+            console.log(data[i].text);
+            console.log(data[i].created_at);
+            // console.log("############################################################");
+
+        };
+    };
+}
+
+//Spotify request
+function spotifyfn() {
+    //console.log("spotify");
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "spotify",
+            message: "What music would you like to search?"
+        }
+
+    ]).then(function(inquirerResponse) {
+
+        var spotify = new Spotify({
+            id: "23d4f43f6f2e45d3be3d2a43d1e639c2",
+            secret: "08debe914cc2428b968d9e13504363b7"
+        });
+
+        spotify
+            .search({ type: 'track', query: 'All the Small Things' })
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        var songName = "";
+        songName = songName + inquirerResponse.spotify;
+
+        spotify.search({ type: 'track', query: songName }, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+
+            console.log(data[0]);
+
+
+        });
+    });
+}
+
+function spotifyTest() {
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "song",
+            message: "What song would you like to search?"
+        }
+    ]).then(function(inquirerResponse) {
+        var Spotify = require('node-spotify-api');
+
+        var spotify = new Spotify({
+            id: "23d4f43f6f2e45d3be3d2a43d1e639c2",
+            secret: "08debe914cc2428b968d9e13504363b7"
+        });
+
+        spotify
+
+        var songName = "";
+        var paramsS = {
+                type: "track",
+                query: songName + inquirerResponse.song
+
+            }
+            .search(paramsS)
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    });
+}
+
+
+//OMBD Request
+function movie() {
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "movie",
+            message: "What movie would you like to search?"
+        }
+    ]).then(function(inquirerResponse) {
+
+        var movieName = "";
+        movieName = movieName + inquirerResponse.movie;
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=40e9cece";
+        request(queryUrl, function(error, response, body) {
+
+            if (!error && response.statusCode === 200) {
+                //Result to display = 
+                console.log("############################################################");
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating :" + JSON.parse(body).Ratings[1].Value);
+                console.log("Country of Origin: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                console.log("############################################################");
+            };
+        });
+    });
+
+}
+
+
+//Choose what to run
+inquirer.prompt([
+
+        {
+            type: "list",
+            message: "What command to run:",
+            choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
+            name: "command"
+        }
+
+    ])
+
+    .then(function(inquirerResponse) {
+
+        //What to do with the choice
+        //switch statement
+        switch (inquirerResponse.command) {
+            case "my-tweets":
+                twitter();
+                break;
+
+            case "spotify-this-song":
+                spotifyTest();
+                break;
+
+            case "movie-this":
+                movie();
+                break;
+
+            case "do-what-it-says":
+                runText();
+                break;
+        }
+    });
